00bca947beba4180dfc57095f216dbf1eb2537d3
Robinhood <okaforchidubem7@gmail.com>
2024-12-26 11:40:38 -0800

    Initialize subgraph

---
+++ createfundsfactory/src/create-funding.ts
@@ -0,0 +1,20 @@
+import { CreateFundingEvent as CreateFundingEventEvent } from "../generated/CreateFunding/CreateFunding"
+import { CreateFundingEvent } from "../generated/schema"
+
+export function handleCreateFundingEvent(event: CreateFundingEventEvent): void {
+  let entity = new CreateFundingEvent(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.creator = event.params.creator
+  entity.issueLink = event.params.issueLink
+  entity.maxAmount = event.params.maxAmount
+  entity.unlockTime = event.params.unlockTime
+  entity.minAmount = event.params.minAmount
+  entity.feePercentage = event.params.feePercentage
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ createfundsfactory/tests/create-funding.test.ts
@@ -0,0 +1,90 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { CreateFundingEvent } from "../generated/schema"
+import { CreateFundingEvent as CreateFundingEventEvent } from "../generated/CreateFunding/CreateFunding"
+import { handleCreateFundingEvent } from "../src/create-funding"
+import { createCreateFundingEventEvent } from "./create-funding-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let creator = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let issueLink = "Example string value"
+    let maxAmount = BigInt.fromI32(234)
+    let unlockTime = BigInt.fromI32(234)
+    let minAmount = BigInt.fromI32(234)
+    let feePercentage = BigInt.fromI32(234)
+    let newCreateFundingEventEvent = createCreateFundingEventEvent(
+      creator,
+      issueLink,
+      maxAmount,
+      unlockTime,
+      minAmount,
+      feePercentage
+    )
+    handleCreateFundingEvent(newCreateFundingEventEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("CreateFundingEvent created and stored", () => {
+    assert.entityCount("CreateFundingEvent", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "CreateFundingEvent",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "creator",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "CreateFundingEvent",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "issueLink",
+      "Example string value"
+    )
+    assert.fieldEquals(
+      "CreateFundingEvent",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "maxAmount",
+      "234"
+    )
+    assert.fieldEquals(
+      "CreateFundingEvent",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "unlockTime",
+      "234"
+    )
+    assert.fieldEquals(
+      "CreateFundingEvent",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "minAmount",
+      "234"
+    )
+    assert.fieldEquals(
+      "CreateFundingEvent",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "feePercentage",
+      "234"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/developer/matchstick/#asserts
+  })
+})
